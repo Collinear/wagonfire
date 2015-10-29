c168f97f5f06b5e9cded986491dea0f9b7e83c10
mpc <mcampbell@elevenfiftyconsulting.com>
2015-10-29 00:51:01 -0400

    Initial MainView screen design

---
+++ wagonfire/MainView/MainViewViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MainViewViewController.swift
+//  wagonfire
+//
+//  Created by mpc on 10/28/15.
+//  Copyright © 2015 mpc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewViewController: UIViewController {
+
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var gsMainView: UIView!
+    @IBOutlet weak var ttMainView: UIView!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
